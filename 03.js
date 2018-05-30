928851d7e21cad0af58c66bc7bf1d4a332750e95
Uthaeus <romanlavery@gmail.com>
2018-05-30 10:45:06 -0600

    String interpolation

---
+++ 03.js
@@ -0,0 +1,5 @@
+// "A long message to (someone's name here) filled with text"
+var msg = "Homer";
+console.log("A long message to " + msg + " filled with text");
+console.log("A long message to " + msg + " filled with text");
+//# sourceMappingURL=03.js.map
---
+++ 03.ts
@@ -0,0 +1,9 @@
+// "A long message to (someone's name here) filled with text"
+
+var msg : string = "Homer"
+
+console.log("A long message to " + msg + " filled with text");
+
+console.log(`A long message to ${msg} filled with text`);
+
+
