133fa3a05926f7e59407785287ce10e02fcb4946
Uthaeus <romanlavery@gmail.com>
2018-05-30 10:40:14 -0600

    Variables in typescript

---
+++ 02.js
@@ -0,0 +1,14 @@
+// var fullName : string = "Homer Simpson";
+// let paidAccount : boolean = true;
+// const versionNumber : number = 1.3;
+// fullName = "Lisa";
+// paidAccount = false;
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+function printName(f, l) {
+    var greeting = "Hi there, ";
+    console.log(greeting + f + ' ' + l);
+}
+printName('Homer', 'Simpson');
+//# sourceMappingURL=02.js.map
---
+++ 02.ts
@@ -0,0 +1,18 @@
+// var fullName : string = "Homer Simpson";
+// let paidAccount : boolean = true;
+// const versionNumber : number = 1.3;
+
+// fullName = "Lisa";
+// paidAccount = false;
+
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+
+
+function printName(f, l) {
+  var greeting : string = "Hi there, ";
+  console.log(greeting + f + ' ' + l);
+}
+
+printName('Homer', 'Simpson');
