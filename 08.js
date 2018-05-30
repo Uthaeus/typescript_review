02ec2afa8937c6e589ec3098e1ac6906d49353f4
Uthaeus <romanlavery@gmail.com>
2018-05-30 11:24:38 -0600

    Conditionals

---
+++ 08.js
@@ -0,0 +1,5 @@
+var password = 'asdfasdf';
+if (password == 'asdfasdf') {
+    console.log('This is the password');
+}
+//# sourceMappingURL=08.js.map
---
+++ 08.ts
@@ -0,0 +1,8 @@
+let password : string = 'asdfasdf';
+
+if (password == 'asdfasdf') {
+  console.log('This is the password');
+} else {
+  console.log('Sorry, permission denied');
+}
+
